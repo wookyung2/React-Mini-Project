9515da75cfdbf7330b653ad47a7db49456366413
wookyung2 <gyong1540@naver.com>
2022-08-26 19:40:36 +0900

    add Router

--- eeagle/src/App.js
+++ eeagle/src/App.js
@@ -1,8 +1,18 @@
+import Main from "./components/Main";
+import Search from "./components/Search";
+import Clip from "./components/Clip";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/clip" element={<Clip />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
