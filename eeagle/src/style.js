8220a580695316581dd2bcacd949e9bf63778a89
ChyunKim <co1995@naver.com>
2022-08-26 17:56:27 +0900

    feat: 스타일 기능 추가
    search,clip 공통된 스타일 부분 추가

---
+++ eeagle/src/style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ListContanier = styled.div`
+  width: 800px;
+  height: 878px;
+  margin: 52px 520px 0 120px;
+`;
+
+export const ListDiv = styled.div`
+  width: 800px;
+  padding: 20px 0;
+`;
+
+export const Title = styled.div`
+  height: 25px;
+  font-size: 20px;
+  color: #0c2d6d;
+  margin-bottom: 12px;
+  font-weight: 500;
+`;
+export const ATag = styled.a`
+  text-decoration-line: none;
+  color: inherit;
+`;
+export const ClipIcon = styled.img`
+  height: 16px;
+  margin: 0 13px;
+`;
+export const Content = styled.div`
+  font-size: 16px;
+  color: #3d3d3d;
+  margin-bottom: 8px;
+`;
