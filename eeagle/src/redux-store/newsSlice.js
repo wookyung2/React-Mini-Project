4632d168ae984c66567d15cc6d046a51a7366ccb
froggy1014 <clcl6084@gmail.com>
2022-08-27 02:27:55 +0900

    feat: Createa AsyncThunk and Add extraReducers
    1. AsyncThunk 생성하고, 처리상태에따른 액션실행코드 입력

--- eeagle/src/redux-store/newsSlice.js
+++ eeagle/src/redux-store/newsSlice.js
@@ -8,12 +8,37 @@ const initialState = {
   page: 1
 };
 
+export const fetchArticle = createAsyncThunk('news/fetchArticle', ({keyword,page}) => {
+  return fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&page=${page}&api-key=vShxWUp7CoaBCrPHDrROH4wxeGZjOtJ7`)
+  .then((res) => res.json())
+  .catch((err) => console.log(err))
+});
+
 const newsSlice = createSlice({
   name: 'news',
   initialState,
   reducers:{
   }, 
+  extraReducers:{
+    [fetchArticle.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [fetchArticle.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      const articles = action.payload.response.docs
+      articles.map((article) => {
+        state.articles.push(article)
+      })
+      state.page = state.page + 1
+    },
+    [fetchArticle.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  }
 })
+
+
+
 
 export const { } = newsSlice.actions;
 export default newsSlice.reducer;
