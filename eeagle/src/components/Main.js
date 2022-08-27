64827f90c8f201456ba333057ff5330ed54bc16d
froggy1014 <clcl6084@gmail.com>
2022-08-27 22:31:06 +0900

    feat: redux-toolkit persist setup
    1. persist setup
    2. 테스트 완료

--- eeagle/src/components/Main.js
+++ eeagle/src/components/Main.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Nav from './Nav'
 
 const Main = () => {
+  const {keyword} = useSelector(state => state.news);
   return (
     <>
       <Nav />
-      <div>Main</div>
+      <div style={{marginTop: '100px'}}>Main</div>
+      <div className="container">
+        {keyword.map((word) => {return <h1>{word}</h1>})}
+      </div>
+      
     </>
   )
 }
