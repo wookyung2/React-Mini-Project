b7fdef1ba727900b4e6588cf8e7acdc02efe670e
froggy1014 <clcl6084@gmail.com>
2022-08-27 20:37:10 +0900

    feat: action 실행코드안에 조건을 더 채워넣음

--- eeagle/src/redux-store/newsSlice.js
+++ eeagle/src/redux-store/newsSlice.js
@@ -2,13 +2,12 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   isLoading: false,
+  clippedId : [],
   clipped : [],
   articles: [],
   keyword : [],
   page: 1
 };
-
-
 
 // 검색한 키워드와 페이지 payload 객체로 받아서, fetch를 돌리는 비동기 액션
 export const fetchArticle = createAsyncThunk('news/fetchArticle', ({keyword,page}) => {
@@ -21,14 +20,21 @@ const newsSlice = createSlice({
   name: 'news',
   initialState,
   reducers:{
-    // 기사 옆 clip버튼 누르면 해당 기사를 클립하는 기능임 
-    // articles에서 id를 비교해서, clipped에 푸시
+    // clipToggle 클릭시, indexOf로 이미 들어가있는 기사인지 확인 진행 후
+    // 토글 기능 수행 
     clipToggle: (state, action) => {
       const articles = state.articles
+      if(state.clippedId.indexOf(action.payload.id) !== -1){
+        const index = state.clippedId.indexOf(action.payload.id);
+        state.clippedId.splice(index, 1)
+        state.clipped.splice(index, 1)
+      }
+      else{
       const clippedArticle = articles.filter(article =>
-          article._id === action.payload.id
-      )  
+          article._id === action.payload.id)  
+      state.clippedId.push(action.payload.id);
       state.clipped.push(...clippedArticle);
+      }
     },
 
     // fetch하게되는 키워드를 키워드를 푸시하는데 그전에
@@ -42,9 +48,14 @@ const newsSlice = createSlice({
       state.keyword = newKeywords;
     },
 
-    // 키워드가 변동되면 articles를 비워줘야함
-    cleanUpArticles: (state)=> {
+    // 키워드가 변동되면 articles를 비워주고,
+    // clipped Articles로 채워준다.
+    // page를 1로 초기화 해준다. 
+    cleanUpArticles: (state, action)=> {
       state.articles = [];
+      state.clipped.map((article)=>{
+        state.articles.push(article);
+      })
       state.page = 1;
     },
   }, 
@@ -54,14 +65,17 @@ const newsSlice = createSlice({
       state.isLoading = true;
     },
     // fetch가 성공적으로 진행되면 isLoading을 false로 해주고,
-    // 받아온 기사들은 articles에 푸시해서 다 넣어줌 
+    // clipped Articles에 이미 클립된 기사라면 push하지 않고
+    // 나머지는 다 넣어준다. 
     [fetchArticle.fulfilled]: (state, action) => {
       state.isLoading = false;
       const articles = action.payload.response.docs
       articles.map((article) => {
+        if(state.clippedId.indexOf(article._id) === -1)
         state.articles.push(article)
       })
       state.page = state.page + 1
+      console.log(state.page);
     },
     // fetch 실패시 isLoading만 일단 false로 만들어줌 
     [fetchArticle.rejected]: (state) => {
