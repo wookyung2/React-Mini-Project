85f4ef633d4ed6ca8fe872e290ce0f25d2a9b8b3
jimin <jim1286@khu.ac.kr>
2022-09-01 19:16:59 +0900

    Feat : V 1.1.4 Bug Fix - add .env, delete console, change class to className

--- eeagle/src/components/Search.js
+++ eeagle/src/components/Search.js
@@ -25,9 +25,8 @@ function Search() {
     <>
       <Nav showClip={false} />
       <ListContanier>
-        {console.log(articleList)}
         {articleList.map((ele) => (
-          <Article ele={ele} />
+          <Article key={ele._id} ele={ele} />
         ))}
         <div ref={ref} />
       </ListContanier>
